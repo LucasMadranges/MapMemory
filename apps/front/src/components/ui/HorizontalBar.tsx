eacb5374df9ad84a9047d0f10d3de37c538c071d
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-01-19 19:36:35 +0100

    :lipstick: fix(HorizontalBar): Update style
    Update margin HorizontalBar.tsx

--- apps/front/src/components/ui/HorizontalBar.tsx
+++ apps/front/src/components/ui/HorizontalBar.tsx
@@ -1,5 +1,5 @@
 export default function HorizontalBar() {
   return (
-    <div className="border-b border-gray-200 rounded-full w-full py-4"></div>
+    <div className="border-b border-gray-200 rounded-full w-full my-8"></div>
   );
 }
