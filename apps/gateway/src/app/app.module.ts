d48281afb3bb219520bd2038a8a4e61ecdc1f315
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-14 19:41:06 +0100

    :sparkles: (Gateway, Users): Adding graphql configuration

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from "@nestjs/common";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloGatewayDriver, ApolloGatewayDriverConfig} from "@nestjs/apollo";
+import {IntrospectAndCompose} from "@apollo/gateway";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+            driver: ApolloGatewayDriver,
+            server: {
+                csrfPrevention: false,
+                debug: process.env.NODE_ENV === "dev",
+                nodeEnv: process.env.NODE_ENV,
+                introspection: true,
+                playground: false,
+            },
+            gateway: {
+                supergraphSdl: new IntrospectAndCompose({
+                    subgraphs: [
+                        {name: "users", url: `http://localhost:${process.env.PORT_USER}/graphql`},
+                    ],
+                    pollIntervalInMs: 1000,
+                }),
+            },
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/users/src/app/app.module.ts
+++ apps/users/src/app/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from "@nestjs/common";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloFederationDriver, ApolloFederationDriverConfig} from "@nestjs/apollo";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
