9455df501a54917af9195cd653be3630d7f780e6
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-14 16:20:49 +0100

    :bug: fix(Button): Better focus

--- apps/front/src/components/buttons/Button.tsx
+++ apps/front/src/components/buttons/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({children, type, onClick, className}: {
 }) {
   return (
     <button onClick={onClick}
-            className={`rounded-full w-full transition ${className} active:scale-95 focus-visible:border-blue-900
+            className={`rounded-full w-full transition ${className} active:scale-95 focus:outline-blue-900 focus:outline
                         ${type === "primary" && "bg-blue-500 text-white hover:bg-blue-600"}
                         ${type === "secondary" && "bg-white text-blue-500 border border-blue-500 hover:bg-gray-100"}
                         ${type === "tertiary" && "bg-white text-black hover:bg-gray-100"}`}>
