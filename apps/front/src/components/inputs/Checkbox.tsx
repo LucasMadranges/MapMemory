54d184895d3771b36c981277a146b749fb205baa
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-14 17:07:28 +0100

    :sparkles: feat(Checkbox): New custom check input

--- apps/front/src/components/inputs/Checkbox.tsx
+++ apps/front/src/components/inputs/Checkbox.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, {useRef} from "react";
-import {gsap} from "gsap";
+import React, {useRef, useState} from "react";
+import {Check} from "../icons/Check";
 
 export default function Checkbox({
                                    name,
@@ -14,33 +14,33 @@ export default function Checkbox({
   inputClassName?: string,
 }) {
   const refCheckbox = useRef(null);
+  const [check, setCheck] = useState(false);
 
-  // Focus animation
-  function handleFocus() {
-    gsap.to(refCheckbox.current, {
-      borderColor: "#1e3a8a",
-      duration: 0.5,
-    });
-  }
-
-  function handleBlur() {
-    gsap.to(refCheckbox.current, {
-      borderColor: "#d1d5db",
-      duration: 0.5,
-    });
+  function handleClick() {
+    setCheck(!check);
   }
 
   return (
     <div className={`flex items-center justify-start gap-2 ${containerClassName}`}>
-      <input name={name}
-             id={name}
-             ref={refCheckbox}
-             onFocus={handleFocus}
-             onBlur={handleBlur}
-             type={"checkbox"}
-             className={`${inputClassName} w-4 h-4 bg-blue-900 rounded-full`}/>
+      <div className={"relative flex items-center justify-center"}>
+        <input ref={refCheckbox}
+               onClick={handleClick}
+               id={name}
+               name={name}
+               type={"checkbox"}
+               className={`${inputClassName} relative w-4 h-4 focus-visible:outline-blue-900 focus-visible:outline cursor-pointer
+               appearance-none rounded border border-blue-500
+               ${check ? "bg-blue-500" : "bg-white"}`}/>
+        {check && <Check onClick={handleClick}
+                         className={"cursor-pointer absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white"}/>}
+      </div>
       <label htmlFor={name}
              className={"inline-block"}>{label}</label>
     </div>
   );
 }
+
+/*
+ {check && <Check onClick={handleClick}
+ className={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white h-4 w-4 cursor-pointer"}/>}
+ */
