64d8f558241cd212ef489b4f4c91ec5dddf6aafb
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-14 16:16:22 +0100

    :bug: fix(Button): Remove text-blue by text-black for tertiary button

--- apps/front/src/components/buttons/Button.tsx
+++ apps/front/src/components/buttons/Button.tsx
@@ -8,10 +8,10 @@ export default function Button({children, type, onClick, className}: {
 }) {
   return (
     <button onClick={onClick}
-            className={`rounded-full w-full transition ${className} active:scale-95
+            className={`rounded-full w-full transition ${className} active:scale-95 focus-visible:border-blue-900
                         ${type === "primary" && "bg-blue-500 text-white hover:bg-blue-600"}
                         ${type === "secondary" && "bg-white text-blue-500 border border-blue-500 hover:bg-gray-100"}
-                        ${type === "tertiary" && "bg-white text-blue-500 hover:bg-gray-100"}`}>
+                        ${type === "tertiary" && "bg-white text-black hover:bg-gray-100"}`}>
       {children}
     </button>
   );
