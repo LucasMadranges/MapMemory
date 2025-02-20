e41530030e97c9c986965c36b4128dd530a4d900
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-20 19:43:50 +0100

    🔒 feat(main.ts): add Helmet middleware for security
    Enabled Helmet middleware in both users and gateway applications to enhance HTTP headers' security. This improves protection against common web vulnerabilities such as XSS and clickjacking.

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -6,16 +6,18 @@
 import {Logger} from "@nestjs/common";
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app/app.module";
+import helmet from "helmet";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = "api";
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT_GATEWAY || 4000;
-    await app.listen(port);
-    Logger.log(
-        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-    );
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  const globalPrefix = "api";
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT_GATEWAY || 4000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
 
 bootstrap();
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,9 +1,11 @@
 import {Logger} from "@nestjs/common";
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app/app.module";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   const globalPrefix = "api";
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT_USER || 4001;
