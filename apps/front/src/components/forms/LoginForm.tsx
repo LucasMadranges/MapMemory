4de62cc0599b9ddd66f08df841f83a210f851e38
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-24 20:46:17 +0100

    🎨 refactor(Login): Extract form logic to LoginForm component
    Separated the login form into a standalone `LoginForm` component for better reusability and cleaner code. Updated `Login` to handle only state management and pass props to the new component.

--- apps/front/src/components/forms/LoginForm.tsx
+++ apps/front/src/components/forms/LoginForm.tsx
@@ -1,5 +1,60 @@
-export default function Page() {
+import H1 from "../titles/H1";
+import HorizontalBar from "../ui/HorizontalBar";
+import Input from "../inputs/Input";
+import {User} from "../icons/User";
+import {Eye} from "../icons/Eye";
+import {DisabledEye} from "../icons/DisabledEye";
+import Checkbox from "../inputs/Checkbox";
+import Button from "../buttons/Button";
+import {Dispatch, MouseEventHandler, SetStateAction} from "react";
+
+type LoginFormProps = {
+  email: string;
+  password: string;
+  setEmail: Dispatch<SetStateAction<string>>;
+  setPassword: Dispatch<SetStateAction<string>>;
+  handleSubmit: MouseEventHandler;
+}
+
+export default function LoginForm({
+                                    email,
+                                    password,
+                                    setEmail,
+                                    setPassword,
+                                    handleSubmit,
+                                  }: LoginFormProps) {
   return (
-    <div>login</div>
+    <form className="bg-white rounded-2xl shadow p-8 md:p-12">
+      <H1>Connexion</H1>
+      <HorizontalBar/>
+      <Input name={"email"}
+             label={"Email"}
+             placeholder={"Email"}
+             value={email}
+             type={"text"}
+             containerClassName={"mb-4"}
+             onChange={(event) => setEmail((event.target as HTMLInputElement).value)}
+             icon={<User width={24}
+                         height={24}/>}/>
+      <Input name={"password"}
+             label={"Mot de passe"}
+             placeholder={"Mot de passe"}
+             value={password}
+             type={"password"}
+             containerClassName={"mb-4"}
+             onChange={(event) => setPassword((event.target as HTMLInputElement).value)}
+             enabledPasswordIcon={<Eye width={24}
+                                       height={24}/>}
+             disabledPasswordIcon={<DisabledEye width={24}
+                                                height={24}/>}/>
+      <Checkbox name={"remember"}
+                label={"Se souvenir de moi"}/>
+      <HorizontalBar/>
+      <Button type={"primary"}
+              className={"py-2"}
+              onClick={handleSubmit}>
+        Se connecter
+      </Button>
+    </form>
   );
 }
--- apps/front/src/components/page/Login.tsx
+++ apps/front/src/components/page/Login.tsx
@@ -1,18 +1,10 @@
 "use client";
-
-import H1 from "../titles/H1";
-import HorizontalBar from "../ui/HorizontalBar";
-import Input from "../inputs/Input";
-import {useState} from "react";
-import {User} from "../icons/User";
-import {Eye} from "../icons/Eye";
-import {DisabledEye} from "../icons/DisabledEye";
-import Checkbox from "../inputs/Checkbox";
-import Button from "../buttons/Button";
 import ContainerSM from "../containers/ContainerSM";
+import LoginForm from "../forms/LoginForm";
+import {useState} from "react";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   // WIP Change any type
@@ -22,38 +14,12 @@ export default function Login() {
 
   return (
     <ContainerSM>
-      <form className="bg-white rounded-2xl shadow p-8 md:p-12">
-        <H1>Connexion</H1>
-        <HorizontalBar/>
-        <Input name={"username"}
-               label={"Nom d'utilisateur"}
-               placeholder={"Nom d'utilisateur"}
-               value={username}
-               type={"text"}
-               containerClassName={"mb-4"}
-               onChange={(event) => setUsername((event.target as HTMLInputElement).value)}
-               icon={<User width={24}
-                           height={24}/>}/>
-        <Input name={"password"}
-               label={"Mot de passe"}
-               placeholder={"Mot de passe"}
-               value={password}
-               type={"password"}
-               containerClassName={"mb-4"}
-               onChange={(event) => setPassword((event.target as HTMLInputElement).value)}
-               enabledPasswordIcon={<Eye width={24}
-                                         height={24}/>}
-               disabledPasswordIcon={<DisabledEye width={24}
-                                                  height={24}/>}/>
-        <Checkbox name={"remember"}
-                  label={"Se souvenir de moi"}/>
-        <HorizontalBar/>
-        <Button type={"primary"}
-                className={"py-2"}
-                onClick={handleSubmit}>
-          Se connecter
-        </Button>
-      </form>
+      <LoginForm
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}/>
     </ContainerSM>
   );
 }
