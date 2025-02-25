42ea3a97102e499657686782f22821f5d6fb0a54
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-02-25 18:55:15 +0100

    🎨 refactor(models): update project configuration to models
    Renamed references from "graphql" to "models" across configuration files. Adjusted display name, paths, and package name to align with the new library name.

--- libs/models/jest.config.ts
+++ libs/models/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'graphql',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
+  displayName: "models",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    "^.+\\.[tj]s$": ["ts-jest", {tsconfig: "<rootDir>/tsconfig.spec.json"}],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/graphql',
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/libs/models",
 };
