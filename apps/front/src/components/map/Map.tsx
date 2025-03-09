4018c6afdc025829388da992ebfee8bfb0f51954
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-09 10:33:16 +0100

    🎨 refactor(Map.tsx): improve Mapbox integration
    Added Mapbox CSS import and moved access token initialization outside the `useEffect`. Updated map center and zoom values, added a marker, and included a cleanup function to remove the map instance. Enhanced styling with a new `z-0` class.

--- apps/front/src/components/map/Map.tsx
+++ apps/front/src/components/map/Map.tsx
@@ -1,6 +1,9 @@
 "use client";
 import mapboxgl from "mapbox-gl";
 import {useEffect, useRef} from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
 export default function Map() {
   // TODO: Delete any
@@ -8,28 +11,33 @@ export default function Map() {
   const map: any = useRef(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-
     map.current = new mapboxgl.Map({
       container: mapRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [0, 0],
-      zoom: 1,
+      center: [2.3522, 48.8566],
+      zoom: 12,
     });
 
     map.current.on("load", () => {
+      const marker1 = new mapboxgl.Marker()
+        .setLngLat([2.3522, 48.8566])
+        .addTo(map.current);
+
       // Disable map rotation using right click + drag
       map.current.dragRotate.disable();
 
       // Disable map rotation using touch rotation gesture
       map.current.touchZoomRotate.disableRotation();
     });
+
+    // Cleanup function
+    return () => map.current?.remove();
   }, []);
 
   return (
     <div className={"w-full h-full py-2 pr-2"}>
       <div ref={mapRef}
-           className={"w-full h-full overflow-hidden rounded-xl"}></div>
+           className={"w-full h-full overflow-hidden rounded-xl z-0"}></div>
     </div>
   );
 }
