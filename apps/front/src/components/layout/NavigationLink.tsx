c0e61b53fee42edae40acdcdb1c1e18cc2577327
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-09 11:16:10 +0100

    🎨 style(NavigationLink.tsx): enhance link hover effects
    Refined hover effects for better responsiveness and visual clarity. Added flex layout and conditional content display based on screen size. Streamlined alignment and hover behavior for a more polished user experience.

--- apps/front/src/components/layout/NavigationLink.tsx
+++ apps/front/src/components/layout/NavigationLink.tsx
@@ -2,17 +2,21 @@ import Link from "next/link";
 import React from "react";
 
 export default function NavigationLink({children, text, href}: {
-    children: React.ReactNode,
-    text: string,
-    href: string
+  children: React.ReactNode,
+  text: string,
+  href: string
 }) {
-    return (
-        <Link href={href}
-              className={`relative transition hover:bg-white [&>svg]:hover:text-black hover:scale-105 z-10 active:scale-95 p-2 rounded-xl
+  return (
+    <Link href={href}
+          className={`relative transition hover:bg-white [&>svg]:hover:text-black hover:scale-105 z-10 active:scale-95 p-2 rounded-xl
+          flex flex-col items-center justify-center gap-2
             focus-visible:outline-blue-900 focus-visible:outline
-            hover:after:content-[attr(data-text)] hover:after:absolute hover:after:top-1/2 hover:after:left-14 hover:after:-translate-y-1/2 hover:after:w-fit hover:after:h-fit hover:after:bg-black hover:after:text-white hover:after:px-2 hover:after:rounded-md hover:after:text-sm`}
-              data-text={text}>
-            {children}
-        </Link>
-    );
+            hover:after:content-[attr(data-text)] hover:after:absolute
+            hover:after:hidden sm:hover:after:block
+            sm:hover:after:top-1/2 sm:hover:after:left-14 sm:hover:after:-translate-y-1/2
+            hover:after:w-fit hover:after:h-fit hover:after:bg-black hover:after:text-white hover:after:px-2 hover:after:rounded-md hover:after:text-sm`}
+          data-text={text}>
+      {children}
+    </Link>
+  );
 }
