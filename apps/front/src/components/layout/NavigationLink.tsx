d3824e4263b730076180708b07e6156f2ae96cd9
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-24 20:57:57 +0100

    🎨 style(components): refine layout and responsive styling
    Adjusted component styles to improve layout consistency and simplify responsive behavior. Removed unnecessary paddings, updated classes for better alignment, and ensured better hover interactions for smaller screens.

--- apps/front/src/components/layout/NavigationLink.tsx
+++ apps/front/src/components/layout/NavigationLink.tsx
@@ -11,7 +11,7 @@ export default function NavigationLink({children, text, href, textDirection, cla
   return (
     <Link href={href}
           className={`relative transition
-          hover:bg-white [&>svg]:hover:text-black
+          sm:hover:bg-white sm:[&>svg]:hover:text-black
           hover:scale-105 z-10 active:scale-95 sm:p-2 rounded-xl
           flex flex-col items-center justify-center gap-2
             focus-visible:outline-blue-900 focus-visible:outline
@@ -21,7 +21,7 @@ export default function NavigationLink({children, text, href, textDirection, cla
             ${textDirection === "top" && "sm:hover:after:bottom-14 sm:hover:after:left-1/2 sm:hover:after:-translate-x-1/2"}
             ${textDirection === "left" && "sm:hover:after:top-1/2 sm:hover:after:right-14 sm:hover:after:-translate-y-1/2"}
             ${textDirection === "right" && "sm:hover:after:top-1/2 sm:hover:after:left-14 sm:hover:after:-translate-y-1/2"}
-            hover:after:w-fit hover:after:h-fit hover:after:bg-black hover:after:text-white hover:after:px-2 hover:after:rounded-md hover:after:text-sm ${className}`}
+            sm:hover:after:w-fit sm:hover:after:h-fit sm:hover:after:bg-black sm:hover:after:text-white sm:hover:after:px-2 sm:hover:after:rounded-md sm:hover:after:text-sm ${className}`}
           data-text={text}>
       {children}
     </Link>
