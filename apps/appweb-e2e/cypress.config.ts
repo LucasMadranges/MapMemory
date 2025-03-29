bc6630b7364269ddd0c7b9f2bf5945c6dc2e0880
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-03-29 18:23:59 +0100

    💡 refactor(workflows): Merge CI and CD workflows into a unified pipeline
    Combined separate CI and CD workflows into a single CI/CD pipeline file for better maintainability and clarity. Updated Dockerfile and Cypress configuration with minor adjustments for consistency and optimization.

--- apps/appweb-e2e/cypress.config.ts
+++ apps/appweb-e2e/cypress.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
     ...nxE2EPreset(__filename, {
       cypressDir: 'src',
       webServerCommands: {
-        default: 'npx nx run front:dev',
+        default: 'npx nx run appweb:dev',
       },
-      ciWebServerCommand: 'npx nx run front:serve-static',
+      ciWebServerCommand: 'npx nx run appweb:serve-static',
       ciBaseUrl: 'http://localhost:3000',
     }),
     baseUrl: 'http://127.0.0.1:3000',
