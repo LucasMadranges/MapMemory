ec7036c938015f0393513e44fba9273a6f87bd2e
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-12-13 21:45:14 +0100

    feat(EventForm, H2, MultiSelect): enhance event form with multi-select and new header
    add a multi-select component for selecting friends in the event form.
    also, replace the h1 tag with a new H2 component for better styling.

--- apps/appweb/src/components/map/EventForm.tsx
+++ apps/appweb/src/components/map/EventForm.tsx
@@ -2,13 +2,13 @@ import { Download } from '../icons/Download';
 import Input from '../inputs/Input';
 import Textarea from '../inputs/Textarea';
 import Button from '../buttons/Button';
+import MultiSelect from '../inputs/MultiSelect';
+import H2 from '../titles/H2';
 
 export default function EventForm() {
   return (
     <div className="flex flex-col items-center p-8 w-[calc(100%-32px-64px-32px)] bg-white overflow-auto h-full rounded-xl">
-      <h1 className="text-yellow text-xl mb-2 text-center">
-        Créer un évènement
-      </h1>
+      <H2>Créer un évènement</H2>
       <div className="p-8 h-48 w-full border border-gray-400 border-dashed rounded-md flex flex-col items-center justify-center gap-4 text-gray-400 mb-4">
         <Download className={'w-8 h-8'} />
         <div className="flex flex-col items-center justify-center">
@@ -26,7 +26,7 @@ export default function EventForm() {
           placeholder={'Ajouter un titre'}
           type={'text'}
           value={''}
-          containerClassName={'mb-2 text-base'}
+          containerClassName={'mb-2'}
           onChange={() => console.log('Changed')}
         />
       </div>
@@ -36,25 +36,12 @@ export default function EventForm() {
           label={'Description'}
           placeholder={'Ajouter une description'}
           value={''}
-          containerClassName={'text-base'}
+          containerClassName={''}
           onChange={() => console.log('Changed')}
         />
       </div>
       <div className="flex flex-col gap-1 w-full mb-4">
-        <label htmlFor="peopleSelect" className="text-base">
-          Personnes
-        </label>
-        <select
-          id="peopleSelect"
-          name="peopleSelect"
-          className="border border-gray-300 rounded-lg w-full p-1 focus-visible:outline-0"
-        >
-          <option value="">Sélectionner une personne</option>
-          <option value="person1">Personne 1</option>
-          <option value="person2">Personne 2</option>
-          <option value="person3">Personne 3</option>
-        </select>
-        <div id="selectedPeople" className="mt-1 hidden"></div>
+        <MultiSelect label={'Amis'} containerClassName={''} name={'friends'} />
       </div>
       <div className="flex flex-col gap-1 w-full mb-4">
         <Input
@@ -63,7 +50,7 @@ export default function EventForm() {
           placeholder={'Ajouter un lieu'}
           type={'text'}
           value={''}
-          containerClassName={'mb-2 text-base'}
+          containerClassName={'mb-2'}
           onChange={() => console.log('Changed')}
         />
       </div>
@@ -74,7 +61,7 @@ export default function EventForm() {
           placeholder={'Ajouter une date'}
           type={'date'}
           value={''}
-          containerClassName={'mb-8 text-base'}
+          containerClassName={'mb-8'}
           onChange={() => console.log('Changed')}
         />
       </div>
---
+++ apps/appweb/src/components/titles/H2.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  weight: '600',
+  subsets: ['latin'],
+});
+
+export default function H2({ children }: { children: React.ReactNode }) {
+  return (
+    <h2
+      className={`${montserrat.className} text-3xl md:text-2xl text-center mb-4`}
+    >
+      {children}
+    </h2>
+  );
+}
