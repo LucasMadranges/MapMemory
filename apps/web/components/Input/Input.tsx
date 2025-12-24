714050df1ef4538dc5fb84b6dd72e8fb6f21e40e
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-12-24 16:06:26 +0100

    feat(login, components): enhance login form with validation
    add state management and validation to the login form.
    implement error handling for email and password inputs using zod schema.

--- apps/web/components/Input/Input.tsx
+++ apps/web/components/Input/Input.tsx
@@ -32,6 +32,16 @@ export default function Input<T extends string | number>({
     }
   }
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = e.target.value;
+    const convertedValue = type === 'number' ? Number(inputValue) : inputValue;
+    setValue(convertedValue as T);
+
+    if (errorMessage && onClearError) {
+      onClearError();
+    }
+  }
+
   function handleShowPassword() {
     setShowPassword(!showPassword);
   }
