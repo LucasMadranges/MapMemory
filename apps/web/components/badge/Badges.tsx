75263068323c0448304aab624d354ea50e214fe6
Lucas Madranges <lucas.madranges.pro@icloud.com>
2026-01-04 18:32:41 +0100

    feat(badges,lateral): add Badges and LateralItem components
    introduce Badges component for displaying labels.
    update Lateral component to include LateralItem, enhancing the layout with additional information.

---
+++ apps/web/components/badge/Badges.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Badges() {
+  return (
+    <span className={'text-white bg-yellow-600 w-fit text-sm rounded-md px-2'}>Restaurant</span>
+  );
+}
--- apps/web/components/layout/Lateral.tsx
+++ apps/web/components/layout/Lateral.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import LateralItem from './LateralItem';
+
 export default function Lateral() {
   return (
-    <div className={'bg-white -ml-4 pl-6 p-2 rounded-r-2xl'}>
-      <div className={'border border-gray-300 rounded-lg'}>Coucou</div>
+    <div className={'bg-white  -ml-4 pl-6 p-2 rounded-r-2xl w-[400px]'}>
+      <LateralItem />
     </div>
   );
 }
